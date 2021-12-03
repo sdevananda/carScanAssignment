45e5a24b7923b33ed0ba0b46e3f0b3a178b45bf4
sdevreddy <devananda@chalo.com>
2021-12-03 18:06:02 +0530

    Adding few constraints for different parameters of user

--- src/main/java/com/example/users/entity/User.java
+++ src/main/java/com/example/users/entity/User.java
@@ -9,6 +9,8 @@ import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -23,8 +25,10 @@ public class User {
     @NotBlank(message="Please add first name")
     private String fname;
     private String lname;
+    @Past(message = "Invalid date")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date dob;
     private String city;
+    @Size(max = 10 , min = 10, message = "Invalid mobile number")
     private String mobile;
 }
